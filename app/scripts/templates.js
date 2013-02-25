419b24e5986250ce47445f4da30e94bf464a3133
Jimmy Oliger <hi@jimmyoliger.com>
2013-02-26 00:34:57 +0100

    Improve admin

--- app/scripts/templates.js
+++ app/scripts/templates.js
@@ -1,1 +1,27 @@
-var foo;
+this["Hull"] = this["Hull"] || {};
+this["Hull"]["templates"] = this["Hull"]["templates"] || {};
+
+this["Hull"]["templates"]["instant_win_admin/instant_admin"] = Handlebars.template(function (Handlebars,depth0,helpers,partials,data) {
+  this.compilerInfo = [2,'>= 1.0.0-rc.3'];
+helpers = helpers || Handlebars.helpers; data = data || {};
+  var buffer = "", stack1, functionType="function", escapeExpression=this.escapeExpression, self=this;
+
+function program1(depth0,data) {
+  
+  var buffer = "", stack1;
+  buffer += "\n          <option value=\""
+    + escapeExpression(((stack1 = depth0.id),typeof stack1 === functionType ? stack1.apply(depth0) : stack1))
+    + "\">"
+    + escapeExpression(((stack1 = depth0.name),typeof stack1 === functionType ? stack1.apply(depth0) : stack1))
+    + " - "
+    + escapeExpression(((stack1 = depth0.id),typeof stack1 === functionType ? stack1.apply(depth0) : stack1))
+    + "</option>\n        ";
+  return buffer;
+  }
+
+  buffer += "<div class=\"hull-instant-admin\">\n  <h1>Instant Win Admin</h1>\n\n  <div class=\"hull-instant-admin__section\">\n    <h2 class=\"hull-instant-admin__section-title\">Create an Instant Win</h2>\n\n    <div>\n      <label for=\"hull-instant-name\">Name: </label>\n      <input id=\"hull-instant-name\" name=\"hull-instant-name\" type=\"text\">\n    </div>\n\n    <div>\n      <label for=\"hull-instant-description\">Description: </label>\n      <input id=\"hull-instant-description\" name=\"hull-instant-description\" type=\"text\">\n    </div>\n\n    <button data-hull-action=\"create\">Create</button>\n  </div>\n\n  <div class=\"hull-instant-admin__section\">\n    <h2 class=\"hull-instant-admin__section-title\">Update Achievement</h2>\n\n    <div>\n      <label for=\"hull-achievement-id\">Select an Instant Win to Edit: </label>\n      <select id=\"hull-achievement-id\" name=\"hull-achievement-id\">\n        ";
+  stack1 = helpers.each.call(depth0, depth0.achievements, {hash:{},inverse:self.noop,fn:self.program(1, program1, data),data:data});
+  if(stack1 || stack1 === 0) { buffer += stack1; }
+  buffer += "\n      </select>\n    </div>\n  </div>\n\n  <div class=\"hull-instant-admin__section\">\n    <div>\n      <label for=\"hull-instant-update-name\">Name: </label>\n      <input id=\"hull-instant-update-name\" name=\"hull-instant-update-name\" type=\"text\">\n    </div>\n\n    <div>\n      <label for=\"hull-instant-update-description\">Description: </label>\n      <input id=\"hull-instant-update-description\" name=\"hull-instant-update-description\" type=\"text\">\n    </div>\n\n    <button data-hull-action=\"updateAchievement\">Update Achievement</button>\n  </div>\n\n  <div class=\"hull-instant-admin__section\">\n    <div>\n      <label for=\"hull-prizes-json\">Prizes JSON: </label>\n      <textarea id=\"hull-prizes-json\" name=\"hull-prizes-json\" rows=\"20\" cols=\"30\"></textarea>\n    </div>\n\n    <button data-hull-action=\"updatePrizes\">Update Prize</button>\n  </div>\n</div>\n";
+  return buffer;
+  });
--- app/widgets/instant_win_admin/main.js
+++ app/widgets/instant_win_admin/main.js
@@ -11,14 +11,29 @@ Hull.widget('instant_win_admin', {
 
   afterRender: function() {
     this.$achievementsSelector = $('#hull-achievement-id');
-    this.$prizesArea = $('#hull-prizes-json');
+    this.$achievementName = $('#hull-instant-update-name');
+    this.$achievementDescription = $('#hull-instant-update-description');
+    this.$achievementPrizes = $('#hull-prizes-json');
 
-    var self = this;
-    this.$achievementsSelector.on('change', _.bind(function() {
-      var id = self.$achievementsSelector.val();
-      self.api('hull/' + id + '/prizes').then(function(res) {
-        self.$prizesArea.val(JSON.stringify({ prizes: res }, null, 2));
-      });
+    this.showAchievement();
+    this.$achievementsSelector.on('change', _.bind(this.showAchievement, this));
+  },
+
+
+  showAchievement: function() {
+    var id = this.$achievementsSelector.val();
+    var achievement = _.where(this.data.achievements, { id: id })[0];
+
+    this.$achievementName.val(achievement.name);
+    this.$achievementDescription.val(achievement.description);
+
+    this.api('hull/' + id + '/prizes').then(_.bind(function(res) {
+      var code = {
+        prizes: _.map(res || [], function(p) {
+          return _.pick(p, 'id', 'name', 'description', 'available_at', 'extra');
+        })
+      };
+      this.$achievementPrizes.val(JSON.stringify(code, null, 2));
     }, this));
   },
 
@@ -27,27 +42,44 @@ Hull.widget('instant_win_admin', {
       e.preventDefault();
 
       this.api('hull/app/achievements', 'post', {
-        'type': 'instant_win',
-        'name': $('#hull-instant-name').val(),
-        'description': $('#hull-instant-description').val()
-      }).then(_.bind(function(res) {
+        type: 'instant_win',
+        name: $('#hull-instant-name').val(),
+        description: $('#hull-instant-description').val()
+      }).done(_.bind(function(res) {
         alert('Instant Win Created');
         this.refresh();
-      }, this));
+      }, this)).fail(function() {
+        alert('Cannot create Instant Win');
+      });
     },
 
-    replacePrizes: function(source, e) {
-      e.preventDefault();
+    updateAchievement: function() {
+      var id = this.$achievementsSelector.val();
+
+      this.api('hull/' + id, 'put', {
+        name: this.$achievementName.val(),
+        description: this.$achievementDescription.val()
+      }).done(function() {
+        alert('Achievement Updated');
+      }).fail(function() {
+        alert('Ooops... Check the form...');
+      });
+    },
+
+    updatePrizes: function() {
+      try {
+        var prizes = $.parseJSON(this.$achievementPrizes.val());
+      } catch (error) {
+        alert('Invalid JSON');
+      }
 
       var id = this.$achievementsSelector.val();
-      try {
-        var prizes = $.parseJSON(this.$prizesArea.val());
-        this.api('hull/' + id + '/prizes', 'put', prizes).then(function() {
-          alert('Prizes updated');
-        });
-      } catch (error) {
-        alert('Invalid json');
-      }
+
+      this.api('hull/' + id + '/prizes', 'put', prizes).done(function() {
+        alert('Prizes updated!');
+      }).fail(function() {
+        alert('Ooops... check your JSON...');
+      });
     }
   }
 });
