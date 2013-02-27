cdf141c26a127329b0c1251d63e24df8af152fc3
Jimmy Oliger <hi@jimmyoliger.com>
2013-02-27 12:05:37 +0100

    Replace working message by a Spinner

--- app/scripts/hull-init.js
+++ app/scripts/hull-init.js
@@ -1,5 +1,18 @@
 Hull.init({
-  appId : "512382f9e85f4250c4000015",
-  orgUrl: "http://hull-demos.alpha.hullapp.io",
+  appId : "512782505cc0175d22000009",
+  orgUrl: "http://develop.hullapp.dev",
   debug : true // optional
+}, function() {
+  Hull.on('hull.instant_win.*.template.render', function() {
+    var el = $('.hull-instant--wokrking')[0];
+    var spinner = Spinner({
+      lines: 13,
+      length: 5,
+      width: 2,
+      radius: 15,
+      color: '#fff',
+      speed: 1.5,
+      opacity: .25
+    }).spin(el);
+  })
 });
