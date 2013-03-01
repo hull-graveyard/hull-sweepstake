531d418d0a5bd0b3f1ee1e21b63b53d3a8ae6c15
Jimmy Oliger <hi@jimmyoliger.com>
2013-03-01 19:00:26 +0100

    Polish things before the release

--- app/scripts/hull-init.js
+++ app/scripts/hull-init.js
@@ -1,18 +1,17 @@
 Hull.init({
-  appId : "512782505cc0175d22000009",
-  orgUrl: "http://develop.hullapp.dev",
-  debug : true // optional
+  appId : '512382f9e85f4250c4000015',
+  orgUrl: 'http://hull-demos.alpha.hullapp.io'
 }, function() {
   Hull.on('hull.instant_win.*.template.render', function() {
     var el = $('.hull-instant--wokrking')[0];
     var spinner = Spinner({
       lines: 13,
       length: 5,
-      width: 2,
+      width: 3,
       radius: 15,
-      color: '#fff',
+      color: '#7c8a9e',
       speed: 1.5,
-      opacity: .25
+      opacity: 0.25
     }).spin(el);
-  })
+  });
 });
