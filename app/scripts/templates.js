fab2351c1eb3e9a46136a318758480d16acf307e
Xavier Cambar <xcambar@gmail.com>
2013-04-12 19:20:36 +0200

    Updates to the newest version

--- app/scripts/templates.js
+++ app/scripts/templates.js
@@ -1,20 +1,76 @@
 this["Hull"] = this["Hull"] || {};
 this["Hull"]["templates"] = this["Hull"]["templates"] || {};
 
-this["Hull"]["templates"]["instant_win/intro"] = Handlebars.template(function (Handlebars,depth0,helpers,partials,data) {
+this["Hull"]["templates"]["instant_win/buttons"] = function (Handlebars,depth0,helpers,partials,data) {
+  this.compilerInfo = [2,'>= 1.0.0-rc.3'];
+helpers = helpers || Handlebars.helpers; data = data || {};
+  var buffer = "", stack1, functionType="function", escapeExpression=this.escapeExpression, helperMissing=helpers.helperMissing, self=this;
+
+function program1(depth0,data) {
+  
+  
+  return "\n  <button class=\"hull-btn\" data-hull-action=\"play\">Play</button>\n";
+  }
+
+function program3(depth0,data) {
+  
+  var buffer = "", stack1;
+  buffer += "\n  ";
+  stack1 = helpers.each.call(depth0, depth0.authProviders, {hash:{},inverse:self.noop,fn:self.program(4, program4, data),data:data});
+  if(stack1 || stack1 === 0) { buffer += stack1; }
+  buffer += "\n";
+  return buffer;
+  }
+function program4(depth0,data) {
+  
+  var buffer = "", stack1, options;
+  buffer += "\n    <button class=\"hull-btn\" data-hull-action=\"play\"\n      data-hull-provider="
+    + escapeExpression((typeof depth0 === functionType ? depth0.apply(depth0) : depth0))
+    + ">Play with ";
+  options = {hash:{},data:data};
+  buffer += escapeExpression(((stack1 = helpers.capitalize),stack1 ? stack1.call(depth0, depth0, options) : helperMissing.call(depth0, "capitalize", depth0, options)))
+    + "</button>\n  ";
+  return buffer;
+  }
+
+  stack1 = helpers['if'].call(depth0, depth0.loggedIn, {hash:{},inverse:self.program(3, program3, data),fn:self.program(1, program1, data),data:data});
+  if(stack1 || stack1 === 0) { buffer += stack1; }
+  buffer += "\n";
+  return buffer;
+  };
+
+this["Hull"]["templates"]["instant_win/intro"] = function (Handlebars,depth0,helpers,partials,data) {
   this.compilerInfo = [2,'>= 1.0.0-rc.3'];
 helpers = helpers || Handlebars.helpers; partials = partials || Handlebars.partials; data = data || {};
   var buffer = "", stack1, self=this;
 
 
-  buffer += "<div class=\"hull-instant hull-instant--intro\">\n  <h1 class=\"hull-instant__title\">Hull Instant Invite</h1>\n  <div class=\"hull-instant__description\">\n    <p>hull is a full stack social platform as a service. It lets you develop social apps in a weekend and is currently in private beta..</p>\n  </div>\n  ";
+  buffer += "<div class=\"hull-instant hull-instant--intro\">\n  <h1 class=\"hull-instant__title\">Hull Instant Invite</h1>\n  <div class=\"hull-instant__description\">\n    <p>hull is a full stack social platform as a service. It lets you develop social apps in a weekend and is currently in private beta..</p>\n    <p>If you want an invite without waiting, play to win an immediate access to the platform.</p>\n  </div>\n  ";
   stack1 = self.invokePartial(partials['instant_win/buttons'], 'instant_win/buttons', depth0, helpers, partials, data);
   if(stack1 || stack1 === 0) { buffer += stack1; }
-  buffer += "\n  <div class=\"hull-instant__description\">\n    <p>If you want an invite without waiting, play to win an immediate access to the platform.</p>\n    <small>By the way, this game is open source and built with hull, <a href=\"http://github.com/hull/hullinstant\">check out how it's done.</a></small>\n  </div>\n</div>\n";
+  buffer += "\n</div>\n";
   return buffer;
-  });
+  };
 
-this["Hull"]["templates"]["instant_win/won"] = Handlebars.template(function (Handlebars,depth0,helpers,partials,data) {
+this["Hull"]["templates"]["instant_win/lost"] = function (Handlebars,depth0,helpers,partials,data) {
+  this.compilerInfo = [2,'>= 1.0.0-rc.3'];
+helpers = helpers || Handlebars.helpers; data = data || {};
+  
+
+
+  return "<div class=\"hull-instant hull-instant--lost\">\n  <div class=\"hull-instant-result\">\n    <h1 class=\"hull-instant-result__title\">You Lose...</h1>\n    <p class=\"hull-instant-result__description\">Come back tomorrow!</p>\n  </div>\n</div>\n";
+  };
+
+this["Hull"]["templates"]["instant_win/played"] = function (Handlebars,depth0,helpers,partials,data) {
+  this.compilerInfo = [2,'>= 1.0.0-rc.3'];
+helpers = helpers || Handlebars.helpers; data = data || {};
+  
+
+
+  return "<div class=\"hull-instant hull-instant--played\">\n  <div class=\"hull-instant-result\">\n    <h1 class=\"hull-instant-result__title\">You've already played today!</h1>\n    <p class=\"hull-instant-result__description\">Come back tomorrow!</p>\n  </div>\n</div>\n";
+  };
+
+this["Hull"]["templates"]["instant_win/won"] = function (Handlebars,depth0,helpers,partials,data) {
   this.compilerInfo = [2,'>= 1.0.0-rc.3'];
 helpers = helpers || Handlebars.helpers; data = data || {};
   var buffer = "", stack1, functionType="function", escapeExpression=this.escapeExpression;
@@ -24,13 +80,13 @@ helpers = helpers || Handlebars.helpers; data = data || {};
     + escapeExpression(((stack1 = ((stack1 = ((stack1 = depth0.badge),stack1 == null || stack1 === false ? stack1 : stack1.data)),stack1 == null || stack1 === false ? stack1 : stack1.signature)),typeof stack1 === functionType ? stack1.apply(depth0) : stack1))
     + "\">Activate my account</a>\n      <p>After signing in, why don't you have a look at some other <a href=\"http://hull.io/gallery/\">cool examples</a>?</p>\n    </div>\n  </div>\n</div>\n";
   return buffer;
-  });
+  };
 
-this["Hull"]["templates"]["instant_win/working"] = Handlebars.template(function (Handlebars,depth0,helpers,partials,data) {
+this["Hull"]["templates"]["instant_win/working"] = function (Handlebars,depth0,helpers,partials,data) {
   this.compilerInfo = [2,'>= 1.0.0-rc.3'];
 helpers = helpers || Handlebars.helpers; data = data || {};
   
 
 
   return "<div class=\"hull-instant hull-instant--wokrking\">\n</div>\n";
-  });
+  };
